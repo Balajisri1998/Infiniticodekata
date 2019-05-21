96d2440a915c791f8c090d43969f597311a8d00b
BALAJI <38466314+Balajisri1998@users.noreply.github.com>
2019-05-22 01:00:10 +0530

    Create ck25.c

---
+++ ck25.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+ int n,q,i;
+ scanf("%d %d",&n,&q);
+ if(n>=q)
+ {
+ printf("Invalid");
+ exit(1);
+ }
+ for(i=n+1;i<q;i++)
+ {
+  if(i%2==0)
+  {
+  printf("%d",i);
+  if(i!=(q-1))
+  printf(" ");
+  }
+ }
+}
