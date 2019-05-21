bc1a3f49276ccc2967d79bf6eef5cd26579e6b90
BALAJI <38466314+Balajisri1998@users.noreply.github.com>
2019-05-21 14:30:25 +0530

    Update ck19.c

--- ck19.c
+++ ck19.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
+#include<stdlib.h>
 void main()
 {
 int a[100],i,n,k,sum=0;
 scanf("%d %d",&n,&k);
+  if(k>n)
+  {
+  printf("Invalid");
+   exit(1);
+  }
 for(i=0;i<n;i++)
 {
   scanf("%d",&a[i]);
