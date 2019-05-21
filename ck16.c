2c4b55839119c5f589df0b535e93b4a6ff918bb7
BALAJI <38466314+Balajisri1998@users.noreply.github.com>
2019-05-21 14:50:16 +0530

    Create ck16.c

---
+++ ck16.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+void main()
+{
+    char a[100];
+    int len,i=0,b,j=0,count;
+    gets(a)
+    len=strlen(a);
+    for(j=0;j<len;j++)
+     {
+	    if(!isdigit(a[i]))
+	    {
+	    printf ("Invalid");
+	    exit(1);
+	    }
+     }
+     b=atoi(a);
+     if(((b%4==0)&&(b%100!=0))||(b%400)==0)
+     printf("Yes");
+     else
+     printf("No");
+}
