7446f3a5a857ec651e5389102b9bf3481916db77
BALAJI <38466314+Balajisri1998@users.noreply.github.com>
2019-05-22 15:27:22 +0530

    Create ck26.c

---
+++ ck26.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+ int t=0,n,z,np=0,i,j;
+ scanf("%d %d",&n,&z);
+ for(j=n+1;j<z;j++)
+ {
+ np=0;
+   for(i=2;i<j;i++)
+   {
+     if(j%i==0)
+     {
+     np=1;
+     break;
+     }
+   }
+  if(np==0)
+  {
+   t++;
+   if(t==1)
+   printf("%d ",j);
+   if(t==2)
+   printf("%d",j);
+   if(t>2)
+   printf(" %d",j);
+  }
+ }
+}
