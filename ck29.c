51743c17b51aa009b67c4a3d9f312d5a3b5d36e3
BALAJI <38466314+Balajisri1998@users.noreply.github.com>
2019-05-23 00:18:33 +0530

    Create ck29.c

---
+++ ck29.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+ unsigned long n,i,f=1;
+ scanf("%lu",&n);
+ if(n<=0)
+ {
+ printf("Invalid");
+ exit(1);
+ }
+ for(i=1;i<=n;i++)
+ {
+  f=f*i;
+ }
+ printf("%lu",f);
+}
