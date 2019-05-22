4c19586cf1c2e9c1a6fe00d7508e6f14a9a12a94
BALAJI <38466314+Balajisri1998@users.noreply.github.com>
2019-05-23 01:53:53 +0530

    Create ck31.c

---
+++ ck31.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+ int a,d,n,i,s=0;
+ scanf("%d %d %d",&n,&a,&d);
+ if(a<=0&&d<=0&&n<=0)
+ {
+  printf("Invalid"); 
+  exit(1); 
+ }
+ s=(n*(2*a+(n-1)*d))/2;
+ printf("%d",s);
+}
