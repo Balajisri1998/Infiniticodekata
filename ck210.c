198c0650e33079f25c290ccb7d4012a4cb033512
BALAJI <38466314+Balajisri1998@users.noreply.github.com>
2019-05-23 01:18:29 +0530

    Update ck210.c

--- ck210.c
+++ ck210.c
@@ -2,20 +2,31 @@
 #include<stdlib.h>
 void main()
 {
- int n,i=0;
+ int n,i,k=0;
  scanf("%d",&n);
- if(n<=0)
+ if(n<0)
  {
  printf("Invalid");
-  exit(1);
+ exit(1);
  }
- while(n>0)
+ if(n==0)
  {
-  i=i+5;
-  if(n==1)
-  printf("%d",i);
-  else
-  printf("%d ",i);
-  n--;
+ printf("0 0 0 0 0");
+ exit(1);
  }
+  for(i=n;i==i;i++)
+  {
+  if(i%n==0)
+  {
+  k=k+1;
+  if(k>1)
+  printf(" %d",i);
+  else
+  printf("%d",i);
+  }
+  if(k==5)
+  {
+  exit(1);
+  }
+  }
 }
