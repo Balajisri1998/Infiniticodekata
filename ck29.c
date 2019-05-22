f3bbff4609807fb6dbb7474bb149f3b8da623ca3
BALAJI <38466314+Balajisri1998@users.noreply.github.com>
2019-05-23 00:22:33 +0530

    Update ck29.c

--- ck29.c
+++ ck29.c
@@ -2,12 +2,17 @@
 #include<stdlib.h>
 void main()
 {
- unsigned long n,i,f=1;
+ unsigned long n,i,f=1,k=1;
  scanf("%lu",&n);
- if(n<=0)
+ if(n<0)
  {
  printf("Invalid");
  exit(1);
+ }
+ if(n==0)
+ {
+ printf("%d",k);
+  exit(1);
  }
  for(i=1;i<=n;i++)
  {
