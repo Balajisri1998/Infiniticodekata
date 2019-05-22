832285aed28f924dda0ea5b99d510109f9e67d1f
BALAJI <38466314+Balajisri1998@users.noreply.github.com>
2019-05-23 04:37:49 +0530

    Update ck38.c

--- ck38.c
+++ ck38.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
+#include<stdlib.h>
 void main()
 {
  int n,a[100],i,j;
  scanf("%d",&n);
+ if(n<=0)
+ {
+ printf("Invalid");
+ exit(1);
+ }
  for(i=0;i<n;i++)
  scanf("%d",&a[i]);
  for(i=0;i<n;i++)
