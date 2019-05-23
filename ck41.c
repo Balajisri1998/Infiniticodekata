0ae2cd601d8a713053d3ea677b9a57372b787afa
BALAJI <38466314+Balajisri1998@users.noreply.github.com>
2019-05-23 05:32:59 +0530

    Create ck41.c

---
+++ ck41.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void main()
+{
+char s[1000];
+int i=0,c=0;
+gets(s);
+while(s[i]!='\0')
+{
+if(s[i]==' ')
+i++;
+c++;
+i++;
+}
+printf("%d",c);
+}
