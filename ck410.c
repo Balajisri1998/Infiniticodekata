bb3b320d744227eff8f30e787dfd96480fb56f38
BALAJI <38466314+Balajisri1998@users.noreply.github.com>
2019-05-23 08:29:28 +0530

    Update ck410.c

--- ck410.c
+++ ck410.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
+#include<stdlib.h>
 void main()
 {
 int t=0,n,i=0,f=1,f1=0;
 clrscr();
 scanf("%d",&n);
+ if(n<=0)
+ {
+ printf("Invalid");
+  exit(1);
+ }
 printf("%d",f);
 for(i=1;i<n;i++)
 {
