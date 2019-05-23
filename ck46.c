4e22030ce93bc89eb1079e58c88c8a5f3771d939
BALAJI <38466314+Balajisri1998@users.noreply.github.com>
2019-05-23 07:29:59 +0530

    Create ck46.c

---
+++ ck46.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+char s[10000];
+int i=0,c=0;
+gets(s);
+while(s[i]!='\0')
+{
+if((s[i]>=33&&s[i]<=47)||(s[i]>=58&&s[i]<=64)||(s[i]>=91&&s[i]<96)||(s[i]>=123&&s[i]<=126))
+c++;
+i++;
+}
+printf("%d",c);
+}
